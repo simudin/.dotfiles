b430951c61857e850314fe885e99dd84184a8728
Hasim Sofaruddin <harudin91@gmail.com>
2024-07-20 20:38:04 +0700

    new nvim config

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,14 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "query", "javascript", "html", "rust", "go", "ruby" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end,
+		configs.setup({
+			auto_install = true,
+			ensure_installed = { "c", "lua", "vim", "query", "javascript", "html", "rust", "go", "ruby" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
