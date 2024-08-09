b26562b5bcb838cfbba659f2e17fae9356840100
Hasim Sofaruddin <harudin91@gmail.com>
2024-08-10 06:58:27 +0700

    added copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          hide_during_completion = true,
+          debounce = 75,
+          keymap = {
+            accept = "<Leader><Tab>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
+      require("copilot.suggestion").toggle_auto_trigger()
+    end,
+  }
+}
