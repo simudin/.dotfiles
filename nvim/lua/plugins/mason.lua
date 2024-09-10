49229ec30fbe77d7d9d2d5b43af4df7389bbc997
Hasim Sofaruddin <harudin91@gmail.com>
2024-09-11 06:27:37 +0700

    update ts_ls config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "clangd" },
+				ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "clangd" },
 			})
 		end,
 	},
@@ -19,7 +19,7 @@ return {
 		config = function()
 			require("lspconfig").lua_ls.setup({})
 			require("lspconfig").rust_analyzer.setup({})
-			require("lspconfig").tsserver.setup({})
+			require("lspconfig").ts_ls.setup({})
       require("lspconfig").clangd.setup({})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
