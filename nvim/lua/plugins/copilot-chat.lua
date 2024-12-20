bd2c9b0f984f77a67e0c1dfa51e95f139e27fc3d
Hasim Sofaruddin <harudin91@gmail.com>
2024-12-20 14:24:34 +0700

    Update copilot-chat

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -3,16 +3,15 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    -- See Commands section for default commands if you want to lazy load on them
     opts = {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
-    build = function()
-      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-    end,
     keys = {
       { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Chat with current buffer" },
     },
