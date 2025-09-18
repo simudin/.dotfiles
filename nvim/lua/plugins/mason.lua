de983b9df24800b36d5ab35a03a16b1c5b6cde4f
Hasim Sofaruddin <harudin91@gmail.com>
2025-09-18 18:25:06 +0700

    update nvim-lspconfig config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,33 +1,33 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "clangd", "gopls", "golangci_lint_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("lspconfig").lua_ls.setup({})
-			require("lspconfig").rust_analyzer.setup({})
-			require("lspconfig").ts_ls.setup({})
-      require("lspconfig").clangd.setup({})
-      require("lspconfig").gopls.setup({})
-      require("lspconfig").golangci_lint_ls.setup({})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "clangd", "gopls", "golangci_lint_ls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("rust_analyzer", {})
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.config("clangd", {})
+      vim.lsp.config("gopls", {})
+      vim.lsp.config("golangci_lint_ls", {})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float, {})
+    end,
+  },
 }
